b6b5c6bab61f9109d6926a76161c72f82c36ea7b
cbertinci <jcbp.jcbp@gmail.com>
2021-05-19 02:51:10 -0300

    feat: add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 import App from './containers/App'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import './custom.scss'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
